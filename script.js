1977b670254c7f1ecbfa79643d77f63157762a62
Hf Wassim <56920664+hfwassim010@users.noreply.github.com>
2024-12-01 00:24:26 +0100

    update saving task operation
    add a new tasks array to store all tasks added to the tasks box, then we can access to each task by task reference from tasks array and then we can handle information or delete the specific task.

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 var taskId = 1
+var tasks = []
 
 function add(){
     let task = document.getElementById("task").value
     let tasksList = document.querySelector(".tasks-list")
     let taskRef = `t-${taskId}`
-    content = 
-    `
-    <div id="${taskRef}" class="task-box">
-        <textarea readonly>${task}</textarea>
-        <div class="btn">
-            <button class="del-btn" onclick="deleteTask('${taskRef}')">Delete</button>
-            <button class="edit-btn" onclick="edit('${taskRef}')">Edit</button>
+    let newTask = {
+        id:taskRef,
+        name:task,
+    }
+    tasks.push(newTask)
+    console.log(tasks)
+    tasksList.innerHTML = ""
+    tasks.map((task) => {
+        content = 
+        `
+        <div id="${task.id}" class="task-box">
+            <textarea readonly>${task.name}</textarea>
+            <div class="btn">
+                <button class="del-btn" onclick="deleteTask('${task.id}')">Delete</button>
+                <button class="edit-btn" onclick="edit('${task.id}')">Edit</button>
+            </div>
         </div>
-    </div>
-    `
-    tasksList.innerHTML += content
+        
+        `
+        tasksList.innerHTML += content
+    })
 
     taskId++
 }
@@ -23,17 +34,25 @@ function add(){
 function deleteTask(id) {
     let element = document.getElementById(id)
     element.remove()
+
+    tasks.filter((task) => {
+        if (task.id != id){
+            return task
+        }
+    })
 }
 
 function clearTasks(){
     let tasksList = document.querySelector(".tasks-list")
     tasksList.innerHTML = ""
+    tasks=[]
 }
 
 function edit(id){
     
     let elements = document.getElementById(id).children
     elements[0].readOnly = false
+
     let elementsBtn = elements[1]
 
     editSaveBtn = elementsBtn.children[1]
@@ -43,9 +62,20 @@ function edit(id){
     
     editSaveBtn.addEventListener("click",()=>{
         if (editSaveBtn.textContent.toLowerCase() == "save"){
+            updatedTask = elements[0].value
+            alert(updatedTask)
+            elements[0].value = updatedTask
             elements[0].readOnly = true
             editSaveBtn.textContent = "Edit"
             editSaveBtn.setAttribute("class","edit-btn")
+
+            tasks.filter((task)=>{
+                if (task.id == id){
+                    task.name = updatedTask
+                }
+                return task
+            })
+
         }else if (editSaveBtn.textContent.toLowerCase() == "edit"){
             elements[0].readOnly = false
             editSaveBtn.textContent = "Save"
